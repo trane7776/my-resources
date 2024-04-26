e19acfa76790bce2e8f03a52b1d244a6edc26a34
trane7776 <izbasarov.t@edu.rea.ru>
2024-04-27 02:37:11 +0300

    fixed typescript error

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
 interface Props {
-  title: string;
   query: string;
   category: string;
 }
 
-const Header = ({ title, query, category }: Props) => {
+const Header = ({ query, category }: Props) => {
   if (query && category) {
     return (
       <h1 className="heading-3 self-start text-white-800">
-        Поиск по "{query}" в <span className="capitalize">{category}</span>
+        Поиск по &quot;query&quot; в{' '}
+        <span className="capitalize">{category}</span>
       </h1>
     );
   }
   if (query) {
     return (
       <h1 className="heading-3 self-start text-white-800">
-        Поиск по "{query}"
+        Поиск по &quot;{query}&quot;
       </h1>
     );
   }
